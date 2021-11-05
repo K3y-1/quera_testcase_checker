37666905e6f2604f989baf64acd4483194f7e2d8
k3y_1 <keyvan_navyek@protonmail.com>
2021-11-05 22:54:07 +0330

    Organize main.py

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 from subprocess import Popen, PIPE
 import os, re, sys
 
-args = sys.argv
-
-if len(args) == 2:
-    EXECUTABLE = args[1]
-else:
-    print('wrong options.')
-    exit(0)
-
-LOG_PERCENTAGE = 100
-PRINT_INPUT = False
-
-input_directory_list = os.listdir('./in')
-output_directory_list = os.listdir('./out')
-
-if len(input_directory_list) != len(output_directory_list):
-    print('there should be same number of input and output files.')
-    exit(0)
-
 def get_input_file_number(string):
-    return int(re.match(r'input(\d+).txt', string).groups()[0])
-
-input_directory_list.sort(key=get_input_file_number)
+    return int(regex.match(string).groups()[0])
 
 def difference(a, b):
     a = a.split('\n')
@@ -50,24 +30,46 @@ def difference(a, b):
     return int(correct_lines / total_lines * 100), (total_lines - correct_lines), a, b
 
 
-for i, inp in enumerate(input_directory_list):
-    out = inp.replace('input', 'output')
-    process = Popen([EXECUTABLE], stdout=PIPE, stdin=PIPE)
-    with open('./in/' + inp, 'r') as in_f:
-        with open('./out/' + out) as out_f:
-            in_text = in_f.read()
-            out_text = out_f.read()
-            output = process.communicate(input=bytes(in_text, 'utf-8'))[0].decode()
-            diff, wrone_lines, output, out_text = difference(output, out_text)
-            if(wrone_lines):
-                print('#' * 70, end='\n\n')
-            print(f'{out} -> {diff}%')
-
-            if diff < LOG_PERCENTAGE:
-                output = '\n'.join(output)
-                out_text = '\n'.join(out_text)
-                print(f'wrong lines: {wrone_lines}')
-                PRINT_INPUT and print('>> input:\n\n' + in_text)
-                print('\n>>> OUTPUT:\n\n' + output)
-                print('\n>>> CORRECT OUTPUT:\n\n' + out_text + '\n\n' + '#' * 70)
             
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) == 2:
+        EXECUTABLE = args[1]
+    else:
+        print('wrong options.')
+        exit(0)
+
+    LOG_PERCENTAGE = 100
+    PRINT_INPUT = False
+
+    input_directory_list = os.listdir('./in')
+    output_directory_list = os.listdir('./out')
+
+    if len(input_directory_list) != len(output_directory_list):
+        print('there should be same number of input and output files.')
+        exit(0)
+
+    regex = re.compile(r'input(\d+).txt')
+    input_directory_list.sort(key=get_input_file_number)
+    
+    for i, inp in enumerate(input_directory_list):
+        out = inp.replace('input', 'output')
+        process = Popen([EXECUTABLE], stdout=PIPE, stdin=PIPE)
+        with open('./in/' + inp, 'r') as in_f:
+            with open('./out/' + out) as out_f:
+                in_text = in_f.read()
+                out_text = out_f.read()
+                output = process.communicate(input=bytes(in_text, 'utf-8'))[0].decode()
+                diff, wrone_lines, output, out_text = difference(output, out_text)
+                if(wrone_lines):
+                    print('#' * 70, end='\n\n')
+                print(f'{out} -> {diff}%')
+
+                if diff < LOG_PERCENTAGE:
+                    output = '\n'.join(output)
+                    out_text = '\n'.join(out_text)
+                    print(f'wrong lines: {wrone_lines}')
+                    PRINT_INPUT and print('>> input:\n\n' + in_text)
+                    print('\n>>> PROGRAM OUTPUT:\n\n' + output)
+                    print('\n>>> CORRECT OUTPUT:\n\n' + out_text + '\n\n' + '#' * 70)
